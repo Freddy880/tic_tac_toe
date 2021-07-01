87a0639a27e3e4424f347bbb56b5c8c6614a577c
Freddy880 <81533091+freddy880@users.noreply.github.com>
2021-07-01 10:28:28 +0200

    Creating of Grid
    - The main Field is created

--- lib/GamePage.dart
+++ lib/GamePage.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Game extends StatefulWidget {
-
   @override
   _GameState createState() => _GameState();
 }
@@ -14,38 +13,83 @@ class _GameState extends State<Game> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            centerTitle: true,
-            leading: Container(),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.power_settings_new),
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-              ),
-              IconButton(
-                  icon: Icon(Icons.refresh),
-                  onPressed: (){},
-              )
-            ],
-            title: Text(
-              "Tic Tac Toe von Flo",
-              style: GoogleFonts.concertOne(),
+      appBar: AppBar(
+          centerTitle: true,
+          leading: Container(),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.power_settings_new),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                    colors: [
-                      Color(0xFF488DFF),
-                      Color(0xFFFF0AE6),
-                    ]
-                ),
-              ),
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
             )
+          ],
+          title: Text(
+            "Tic Tac Toe von Flo",
+            style: GoogleFonts.concertOne(),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.centerRight,
+                  end: Alignment.centerLeft,
+                  colors: [
+                    Color(0xFF488DFF),
+                    Color(0xFFFF0AE6),
+                  ]),
+            ),
+          )),
+      body: _gameBody(),
+    );
+  }
+
+  _gameBody() {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 100,
         ),
+        _gameBoard(),
+        Container(),
+      ],
+    );
+  }
+
+  _gameBoard() {
+    return Expanded(
+        child: GridView.builder(
+          padding: EdgeInsets.all(10),
+          itemCount: 9,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+            ),
+            physics: new NeverScrollableScrollPhysics(),
+            itemBuilder: (context, i) {
+              return Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Color(0x992A2A2A)
+                  )
+                ),
+                child: GestureDetector(
+                  onTap: (){
+                    print("Hi $i");
+                  },
+                  child: Center(
+                    child: Text(
+                      "$i",
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ),
+              );
+            }
+        )
     );
   }
 }
