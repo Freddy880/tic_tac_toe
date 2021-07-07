9d46d2a9b92feb6f2c8b0e2b67e74a593b46562d
Freddy880 <81533091+Freddy880@users.noreply.github.com>
2021-07-07 11:09:30 +0200

    make Settings stateful Widget

--- lib/SettingsPage.dart
+++ lib/SettingsPage.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SettingPage extends StatelessWidget {
+class SettingPage extends StatefulWidget {
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
